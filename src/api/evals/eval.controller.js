3aee22826fe4dea2985cae125435f7648a164455
alexandreMartinEcl <alexandre.martin.ecl@gmail.com>
2018-02-27 12:00:16 +0100

    rectified response for update_eval

--- src/api/evals/eval.controller.js
+++ src/api/evals/eval.controller.js
@@ -5,16 +5,22 @@ module.exports = {};
 
 module.exports.update = (req, res) => {
     console.log("Try eval update");
-    
     Eval.update(
-        {eval_mail: req.user.email, activated: true},
+        {eval_email: req.user.email, activated: "true"},
         { $set: {eval: req.body.eval, updated: true}},
-        (err, answer) => {
-            console.log("ff");
+        (err, eval) => {
+            console.log(eval);
             if (err) {
                 return res.status(500).json(err);
             }
-            let oTemp = {'status': 'ok'};
+
+            let oTemp = {status: "", res: eval};
+
+            if (eval.n !== 1){
+                oTemp.status = "failed";
+            } else {
+                oTemp.status = "ok";
+            }
             res.status(200).json(oTemp);
         });
 };
